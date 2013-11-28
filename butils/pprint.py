f9a15c260cc53eab378b2150dea682eed5673f70
ssbunyk <tbunyk@softserveinc.com>
2013-11-28 12:44:10 +0200

    fixed xml printing

--- butils/pprint.py
+++ butils/pprint.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 '''
 Module for colorfull pretty printing different data.
 
@@ -6,28 +8,30 @@ allow easy inserting print function into expressions.
 '''
 
 from difflib import ndiff
- 
-colors ={
+import xml.dom.minidom as minidom
+from pygments import highlight
+from pygments.lexers import XmlLexer
+from pygments.formatters import TerminalFormatter
+
+colors = {
+    'red': 91,
+    'green': 92,
     '-':91, # for diffs
     '+':92,
     '?':94,
 }
 
-import xml.dom.minidom
-from pygments import highlight
-from pygments.lexers import XmlLexer
-from pygments.formatters import TerminalFormatter
-
 def xml(text):
-    xml = xml.dom.minidom.parseString(text)
-    print highlight(xml.toprettyxml(), XmlLexer(), TerminalFormatter())
+    tree = minidom.parseString(text)
+    print(highlight(tree.toprettyxml(), XmlLexer(), TerminalFormatter()))
     return text
 
-def print_diff(a,b):
+def print_diff(a, b):
+    raise NotImplementedError
     a_lines = render_json(a).splitlines(1)
     b_lines = render_json(b).splitlines(1)
     diff = ndiff(a_lines,b_lines)
  
     for line in diff:
         # TODO: may be use some DiffLexer if such exists?
-        print '\033[%sm%s\033[0m'% (colors.get(line[0],0), line[:-1])
+        print('\033[%sm%s\033[0m'% (colors.get(line[0],0), line[:-1]))
